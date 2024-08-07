d8d2f35e94eef862fc7434e338ffbe84a11aae5c
Robert Todea <robert.todea@gmail.com>
2024-08-07 16:11:12 +0300

    WIP

--- src/SpeechRecognition.java
+++ src/SpeechRecognition.java
@@ -1,6 +1,10 @@
 import com.microsoft.cognitiveservices.speech.*;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -10,16 +14,60 @@ public class SpeechRecognition {
     private static String speechRegion = System.getenv("SPEECH_REGION");
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        setUpProxy();
+        // echoRequest();
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(speechKey, speechRegion);
         speechConfig.setSpeechRecognitionLanguage("en-US");
-        recognizeFromMicrophone(speechConfig);
+        recognizeFromWav(speechConfig);
     }
 
-    public static void recognizeFromMicrophone(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
-        AudioConfig audioConfig = AudioConfig.fromWavFileInput("./samples/one-voice-some-static.wav");
+    private static void setUpProxy() {
+        System.setProperty("https.proxyHost", "localhost");
+        System.setProperty("https.proxyPort", "8888");
+    }
+
+    private static void echoRequest() {
+        try {
+            // The URL you want to fetch
+            String urlString = "https://echo.free.beeceptor.com";
+
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+
+            // Get the response code 
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                // Read the response data
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = reader.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                reader.close();
+
+                System.out.println(response.toString());
+            } else {
+                System.out.println("GET request failed with response code: " + responseCode);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void recognizeFromWav(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+        String sampleFile = "./samples/one-voice-some-static.wav";
+        AudioConfig audioConfig = AudioConfig.fromWavFileInput(sampleFile);
         SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-        System.out.println("Speak into your microphone.");
+        System.out.println("Reading from " + sampleFile);
         Future<SpeechRecognitionResult> task = speechRecognizer.recognizeOnceAsync();
         SpeechRecognitionResult speechRecognitionResult = task.get();
 
